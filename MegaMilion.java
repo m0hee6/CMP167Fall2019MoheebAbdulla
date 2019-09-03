7681128eb05b57e033cf022430805d623ada9a93
m0hee6 <54866738+m0hee6@users.noreply.github.com>
2019-09-03 16:55:06 -0400

    Add files via upload

---
+++ MegaMilion.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @author Moheeb
+ * description:
+ *
+ */
+
+public class MegaMilion {
+	public static void main(string[] args) {
+		Random randGen= new Random();
+		
+		System.out.print(randGen.nextInt);
+		int rnum1=
+		
+		//Decare Varaiable to hold amount of dollar
+		//Declare Variable fot cost of ticket
+		
+		//Propt the user for the amount 
+		// of dollars to bet on
+		
+		// Store amount into amount of dollar
+		//	variable
+		
+		//prompt the user ti enter six numbers 
+		//	create a variable for each value 
+		
+		//Randomly genrate six number  and comaprte
+	}
+}
